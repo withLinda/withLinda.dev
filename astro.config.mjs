41d24b4bfe105055e4e908553509581bd44e8e07
Linda <linda.fitriani@live.com>
2025-01-08 21:56:12 +0700

    now support mdx and gist (embedding)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,25 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-
 import tailwind from '@astrojs/tailwind';
-
 import sitemap from '@astrojs/sitemap';
+import mdx from '@astrojs/mdx';
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://your-site.com/',
-  integrations: [react(), tailwind(), sitemap()]
+  site: 'https://withlinda.dev/',
+  integrations: [
+    react(),
+    tailwind(),
+    sitemap(),
+    mdx({
+      extendMarkdownConfig: true,
+      gfm: true,
+      optimize: true,
+      // Add support for processing JSX in MDX
+      jsx: true,
+      // Enable remark plugins if needed
+      remarkPlugins: [],
+      // Enable rehype plugins if needed
+      rehypePlugins: []
+    })
+  ]
 });
